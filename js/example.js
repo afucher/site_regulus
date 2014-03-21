bb3fe3dd6a0c513a8ad1b0b51f34a75da8bf5b8e
Arthur Fucher <arthurfucher@gmail.com>
2014-03-21 13:21:52 -0300

    adicionado destaque

--- js/example.js
+++ js/example.js
@@ -1,35 +1,32 @@
 angular.module('regulus', ['ui.bootstrap']);
 function AccordionDemoCtrl($scope) {
-  $scope.oneAtATime = false;
-
-  $scope.groups = [
-    {
-      title: "Dynamic Group Header - 1",
-      content: "Dynamic Group Body - 1"
-    },
-    {
-      title: "Dynamic Group Header - 2",
-      content: "Dynamic Group Body - 2"
-    }
-  ];
-
-  $scope.cursos = [
-    {
-      title: "Palestra Aberta",
-      subtitle: "Assim na Terra como no Céu",
-      author: "Patrícia Valente",
-      description: "Como a Astrologia e o conhecimento do próprio mapa Astrológico permite nos conectarmos com o Divino em nós"
-    },
-    {
-      title: "Titulo",
-      subtitle: "Sub-titulo",
-      author: "Autor",
-      description: "Uma breve descrição do que é!"
-    },
-    {
-      title: "Titulo",
-      author: "Autor",
-      description: "Uma breve descrição do que é!"
-    }
-  ];
+    'use strict';
+    $scope.oneAtATime = false;
+    $scope.destaque = {
+        title: "50% de desconto em livros",
+        description: "Venha ver os livros que estão com desconto!",
+        button: {
+            title: "Saber mais...",
+            link: "/livraria.html"
+        }
+    };
+    $scope.cursos = [
+        {
+            title: "Palestra Aberta",
+            subtitle: "Assim na Terra como no Céu",
+            author: "Patrícia Valente",
+            description: "Como a Astrologia e o conhecimento do próprio mapa Astrológico permite nos conectarmos com o Divino em nós"
+        },
+        {
+            title: "Titulodasdads",
+            subtitle: "Sub-titulo",
+            author: "Autor",
+            description: "Uma breve descrição do que é!"
+        },
+        {
+            title: "Titulo",
+            author: "Autor",
+            description: "Uma breve descrição do que é!"
+        }
+    ];
 }
